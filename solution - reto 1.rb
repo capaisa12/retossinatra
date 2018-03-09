0ed946a22c7887e669f23a9e04c70c68383cf27f
capaisa12 <capaisa12@gmail.com>
2018-03-09 14:19:54 -0500

    Desarrollando Reto 2 - Sinatra, se renombra archivo solution del reto 1

---
+++ solution - reto 1.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+
+get '/' do
+  if (params[:nombre]==nil || params[:nombre]=={} || params[:nombre]=="")
+    "Hola desconocido!"
+  else
+    "<h1>Hola #{params[:nombre]}!</h1>"
+  end
+end
--- solution.rb
+++ solution.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 
 get '/' do
+  "Hola desconocido!"
+end
+
+
+get '/makers' do
+  "Hola desconocido!"
+end
+
+get '/makers/:nombre' do
   if (params[:nombre]==nil || params[:nombre]=={} || params[:nombre]=="")
     "Hola desconocido!"
   else
-    "<h1>Hola #{params[:nombre]}!</h1>"
+    "<h1>Hola "+"#{params[:nombre]}!</h1>".capitalize
   end
 end
